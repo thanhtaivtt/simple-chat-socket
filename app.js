f90f3f3ed42e0a1065018bf72bd825daa79ad1f4
Vu Thanh Tai <thanhtai96nd@gmail.com>
2017-11-10 10:35:36 +0700

    change ver

--- app.js
+++ app.js
@@ -1,43 +1,42 @@
 // require express
-var express=require('express');
+var express = require('express');
 // create exxpress
-var app=express();
+var app = express();
 // creat sever
-var http=require('http').Server(app);
+var http = require('http').Server(app);
 // create static
 app.use(express.static('public'));
 // view engine
-app.set('view engine','hbs');
+app.set('view engine', 'ejs');
 // set views
-app.set('views','./views');
+app.set('views', './views');
 // requrie socketio
-var io= require('socket.io')(http);
+var io = require('socket.io')(http);
 // router /
-app.get('/',function(req,res){
-	res.render('index');
+app.get('/', function(req, res) {
+    res.render('index');
 })
 //creat array;
 var arr = [];
-var user=[];
+var user = [];
 // event connect
-io.on("connection", function(socket){
-  console.log("Co nguoi ket noi! ");
-  socket.on('send_user',function(dt){
-  		user.push(dt);
-  		socket.user=dt
-  		console.log(dt);
-  		});
-  socket.on("send_data", function(data){
-    arr.push(data);
-    io.sockets.emit("data_send",  {ct:data,ur:socket.user});
-    console.log(arr);
-  	});
+io.on("connection", function(socket) {
+    console.log("connected: " + socket.id);
+    socket.on('send_user', function(dt) {
+        user.push(dt);
+        socket.user = dt
+        console.log(dt);
+    });
+    socket.on("send_data", function(data) {
+        arr.push(data);
+        io.sockets.emit("data_send", { ct: data, ur: socket.user });
+    });
 
-  
+
 });
-io.on('disconnect', function () {
-		console.log("ngat");
+io.on('disconnect', function() {
+    console.log("ngat");
 
-  });
+});
 //port 3000
-http.listen(process.env.PORT||3000);
+http.listen(process.env.PORT || 8000);
